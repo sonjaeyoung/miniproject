e85868d98d2555720986fe597591f853f14d2029
sonjaeyoung <jyson1987@gmail.com>
2021-05-07 19:05:54 +0900

    commit

---
+++ mini-1/src/test/java/com/web/mini/Mini1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mini1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
